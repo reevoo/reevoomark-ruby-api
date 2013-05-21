6078b9f9cdaca7af5902ee9664cce175c3e40c5a
Tom Lea <commit@tomlea.co.uk>
2013-05-21 16:28:34 +0100

    Redundant require.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,12 @@ $LOAD_PATH << File.expand_path('../../lib', __FILE__)
 require 'rubygems'
 require 'pry'
 require 'rspec'
-require 'webmock'
 require 'webmock/rspec'
 require 'reevoomark'
 
 # Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
 
 RSpec.configure do |config|
-
   config.before do
     FileUtils.rm Dir.glob('tmp/cache/*')
   end
